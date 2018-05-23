ecc218c904772f11c128846179372c45a4f18ada
Ella Musina <ella@Ellas-MacBook-Pro.local>
2018-05-23 11:54:21 -0700

    Adding gulp concatenation for js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 var gulp = require('gulp'),
-  gutil = require('gulp-util');
+  gutil = require('gulp-util'),
+  concat = require('gulp-concat');
+
+var jsFiles = [
+  'builds/dev/js/modernizr-custom.js',
+  'builds/dev/js/main.js'
+];
 
 gulp.task('log', function(){
   gutil.log('US Army Corp Eng Running >');
+});
+
+gulp.task('js', function(){
+  gulp.src(jsFiles)
+    .pipe(concat('prod.js'))
+    .pipe(gulp.dest('builds/dev/js'))
 });
